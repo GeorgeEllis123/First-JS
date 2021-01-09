49113b061514a9744c18be26ef22abff912994db
roi_hibou <ellisvoorhies@gmail.com>
2021-01-09 15:46:05 -0800

    added photos

--- myScript.js
+++ myScript.js
@@ -4,18 +4,26 @@ console.log(myDiv);
 
 // Click event to attach to button
 function myClick () {
-  // Quick check to verify that the function executes.
-  console.log("test function");
   // Get the values that were input into the two text boxes.
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
-  console.log(fname, lname);
+
   // By using = we *replace* the entire contents of the div tag.
   myDiv.innerHTML = "\n";
-  // Now, using += we are *appending* to the new contents of the div tag.
-  myDiv.innerHTML += "\t\t<h1>Whoa.</h1>\n"
-  // Notice mixing of quotation marks, just like in Python.
-  myDiv.innerHTML += "\t\t<img src='default.jpeg' />\n";
+
+  if (document.getElementById("dog").checked) {
+    myDiv.innerHTML += "\t\t<img src='dog.jpg' />\n";
+  }
+  if (document.getElementById("cat").checked) {
+    myDiv.innerHTML += "\t\t<img src='cat.jpg' />\n";
+  }
+  if (document.getElementById("bird").checked) {
+    myDiv.innerHTML += "\t\t<img src='bird.jpeg' />\n";
+  }
+  if (document.getElementById("hamster").checked) {
+    myDiv.innerHTML += "\t\t<img src='hamster.jpg' />\n";
+  }
+
   // Notice here that we are appending the values of the variables.
   myDiv.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
 }
